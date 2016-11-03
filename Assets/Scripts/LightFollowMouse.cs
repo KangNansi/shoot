64271613bb3bc95a00a9db758ac0592db0636bf6
KangNansi <evillz@hotmail.com>
2016-11-03 23:07:10 +0100

    no message

---
+++ Assets/Scripts/LightFollowMouse.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFollowMouse : MonoBehaviour {
+
+	private Transform m_Transform;
+	// Use this for initialization
+	void Start () {
+		m_Transform = GetComponent<Transform>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		mousePosition.z = 0;
+	  m_Transform.LookAt(mousePosition);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D m_Rigidbody2D;
+	private float m_MaxSpeed;
+	// Use this for initialization
+	void Start () {
+		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+		m_MaxSpeed = 5f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float hor = Input.GetAxis("Horizontal");
+		float ver = Input.GetAxis("Vertical");
+
+		m_Rigidbody2D.velocity = new Vector2(hor*m_MaxSpeed, ver*m_MaxSpeed);
+	}
+}
